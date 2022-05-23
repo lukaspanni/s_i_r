29aba8edd18cd31aa6becd1a3659fa37a860f593
Lukas Panni <lukas-panni@web.de>
2022-05-23 07:24:21 +0000

    Add limits

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ b = 0.1
 k = 0.05
 n = 100
 
-s = [0.99*n]
-i = [0.01*n]
-r = [0*n]
+s = [0.99 * n]
+i = [0.01 * n]
+r = [0 * n]
 
-beta = b/n
+beta = b / n
+
 
 def s_fun(t: int):
     return s[t] - beta * i[t] * s[t]
 
+
 def i_fun(t: int):
     return i[t] + beta * i[t] * s[t] - k * i[t]
+
 
 def r_fun(t: int):
     return r[t] + k * i[t]
 
+
 def loop(limit_t: int):
-    for t in range(limit_t-1):
-        s.append(s_fun(t))
-        i.append(i_fun(t))
-        r.append(r_fun(t))
+    for t in range(limit_t - 1):
+        s.append(max(0, min(s_fun(t), n)))
+        i.append(max(0, min(i_fun(t), n)))
+        r.append(max(0, min(r_fun(t), n)))
+
 
 if __name__ == "__main__":
     t = int(input("Iterations:"))
